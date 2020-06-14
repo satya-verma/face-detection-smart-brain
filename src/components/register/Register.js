62cd59d43ac396334941d43c8bac5368c71d1f53
Satyaprakash verma <59075705+blue-run@users.noreply.github.com>
2020-06-14 15:15:35 +0530

    signUp added and state change for different screens added

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const SignIn = ({ onRouteChange }) => {
+const Register = ({ onRouteChange }) => {
     return (
         <article className="br3 ba dark-gray b--black-10 mv5 w-90 w-50-m w-25-l mw-100 center shadow-5">
             <main className="pa4 black-80">
                 <form className="measure">
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                        <legend className="f1 b fw6 ph0 mh0">Sign In</legend>
+                        <legend className="f1 b fw6 ph0 mh0">Sign Up</legend>
                         <div className="mt3">
                             <label className="db fw6 lh-copy f6 " htmlFor="email-address">Email</label>
                             <input className="pa2 b--black input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" autoComplete="username" />
@@ -17,15 +17,15 @@ const SignIn = ({ onRouteChange }) => {
                         </div>
                     </fieldset>
                     <div className="">
-                        <input onClick={() => onRouteChange('home')} className="b br1 ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+                        <input onClick={() => onRouteChange('home')} className="b br1 ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Register" />
                     </div>
                     <div className="lh-copy mt3">
-                        <p href="#0" onClick={() => onRouteChange('register')} className="f6 link dim black db" style={{ cursor: 'pointer' }}>Register</p>
+                        <p href="#0" onClick={() => onRouteChange('signin')} className="f6 link dim black db" style={{ cursor: 'pointer' }}>Sign In</p>
                     </div>
                 </form>
             </main>
         </article>
-    );
+    )
 }
 
-export default SignIn;
+export default Register;
--- src/containers/App.js
+++ src/containers/App.js
@@ -7,12 +7,15 @@ import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import FaceRecognition from '../components/face_recognition_box/FaceRecognition';
 import SignIn from '../components/sign_in/SignIn';
+import Register from '../components/register/Register';
 import '../containers/App.css';
 
+// initialising access for our app to Clarifai api with our Clarifai api key.
 const app = new Clarifai.App({
   apiKey: '9667ec91c3ac444e81d28a6dd4b5e84c'
 })
 
+// live particles component background
 const particlesParam = {
   particles: {
     number: {
@@ -35,13 +38,18 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin'
+      input: '',        // url input 
+      imageUrl: '',     // url input change
+      box: {},          // pixel values for bounding-box
+      route: 'signin'   // screens: signIn, signup & home
     }
   }
 
+  /* 
+    calculateFaceLocation converts the coordinates received, into pixels(px) and returns all values in an object
+    which are then used as styles to draw the bounding-box around the detected face.
+    the (data) parameter gets the response as argument which is returned from api call in the onSubmit function.
+  */
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
@@ -55,42 +63,71 @@ class App extends React.Component {
     }
   }
 
+  /* 
+  displayFaceBox method changes the state of box object with newly received value from the 
+  calculateFaceLocation function
+  */
   displayFaceBox = (box) => {
     this.setState({ box: box });
   }
 
+  // detects the change in input url and changes the input state accordingly
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
   }
 
+  /* 
+  on submitting image-url, Clarifai api is called reads the image and returns the `response` and that `response` 
+  holds the coordinates of the bounding-box for the faces. this `response` is passed to calculateFaceLocation 
+  function as a parameter to convert into pixels and calculateFaceLocation function is then passed as parameter
+  to the displayFaceBox fucntion. 
+  */
   onSubmit = (event) => {
-    this.setState({ imageUrl: this.state.input })
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    const { input } = this.state;
+    this.setState({ imageUrl: input })
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, input)
       .then((response) => this.displayFaceBox(this.calculateFaceLocation(response)))
       .catch(err => console.log(err));
   }
 
+  // this function changes the state of route
   onRouteChange = (route) => {
     this.setState({ route: route })
   }
 
+  /*  
+  receives a {state.route} parameter and returns signIn, signUp or Home page components accordingly which 
+  are then rendered inside the render fucntion.
+  */
+  renderSwitch = (param) => {
+    const { imageUrl, box } = this.state;
+    switch (param) {
+      case 'home':
+        return (
+          <div>
+            <Navigation onRouteChange={this.onRouteChange} />
+            <Logo />
+            <Rank />
+            <ImageURLForm inputChange={this.onInputChange} onSubmit={this.onSubmit} />
+            <FaceRecognition imageSource={imageUrl} box={box} />
+          </div>
+        );
+      case 'signin':
+        return (<SignIn onRouteChange={this.onRouteChange} />);
+      case 'register':
+        return (<Register onRouteChange={this.onRouteChange} />);
+      default: break;
+    }
+  }
+
+  // render 
   render() {
+    const { route } = this.state;
     return (
       <div className="App tc">
         <Particles className="particles tc"
           params={particlesParam} />
-        {this.state.route === 'signin' ?
-          <SignIn onRouteChange={this.onRouteChange} /> :
-          (
-            <div>
-              <Navigation onRouteChange={this.onRouteChange} />
-              <Logo />
-              <Rank />
-              <ImageURLForm inputChange={this.onInputChange} onSubmit={this.onSubmit} />
-              <FaceRecognition imageSource={this.state.imageUrl} box={this.state.box} />
-            </div>
-          )
-        }
+        {this.renderSwitch(route)}
       </div>
     );
   }
